3afc82d19fa678c7e906d8e219be27c68723ba25
IG <11618271+stereoknife@users.noreply.github.com>
2024-11-25 10:09:28 +0100

    java implementation

--- impl/Neighbor.java
+++ impl/Neighbor.java
@@ -7,17 +7,18 @@ public class Neighbor extends Thread {
     public void run() {
         while (true) {
             try {
-                //String name = ... ;
+                String name = Thread.currentThread().getName() ;
+                String other = name.equals("alice") ? "bob" : "alice";
                 System.out.println("try again, my name is: "+ name);
                 Thread.sleep((int)(200*Math.random()));
-                //flags. ...;
-                if ( ... ) {
+                flags.set_true(name);
+                if (!flags.query_flag(other)) {
                     System.out.println(name + " enter");
                     Thread.sleep(400);
                     System.out.println(name + " exits");
                 }
                 Thread.sleep((int)(200*Math.random()));
-                flags ... ;
+                flags.set_false(name); ;
             }
             catch (InterruptedException e) {};
         }
