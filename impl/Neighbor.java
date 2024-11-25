50cd84116c15b68e6e14935db1bca6cb3ca6c654
IG <11618271+stereoknife@users.noreply.github.com>
2024-11-25 13:09:46 +0100

    utilitza la flag correcta

--- impl/Neighbor.java
+++ impl/Neighbor.java
@@ -8,17 +8,16 @@ public class Neighbor extends Thread {
         while (true) {
             try {
                 String name = Thread.currentThread().getName() ;
-                String other = name.equals("alice") ? "bob" : "alice";
                 System.out.println("try again, my name is: "+ name);
                 Thread.sleep((int)(200*Math.random()));
                 flags.set_true(name);
-                if (!flags.query_flag(other)) {
+                if (!flags.query_flag(name)) {
                     System.out.println(name + " enter");
                     Thread.sleep(400);
                     System.out.println(name + " exits");
                 }
                 Thread.sleep((int)(200*Math.random()));
-                flags.set_false(name); ;
+                flags.set_false(name);
             }
             catch (InterruptedException e) {};
         }
